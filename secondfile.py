877eafcc012568cf424f3b36cbde6b1f84734a17
niklasmannhardt <106990882+niklasmannhardt@users.noreply.github.com>
2022-06-13 13:18:59 -0700

    Update secondfile.py

--- secondfile.py
+++ secondfile.py
@@ -9,3 +9,15 @@ if 1654543717 == 1654543717:
 if 1654543717 == 1654543717:
     print("fourth times the charm")
 client("s3", aws_secret_access_key="fakefakefake1654543717fakefake")
+
+import random
+print(1654543717)
+if 1654543717 == 1654543717:
+    print("got here")
+if 1654543717 == 1654543717:
+    print("got here twice")
+if 1654543717 == 1654543717:
+    print("got here thrice")
+if 1654543717 == 1654543717:
+    print("fourth times the charm")
+client("s3", aws_secret_access_key="fakefakefake1654543717fakefake")
